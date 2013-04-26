a65dd4ead88d333dfde55b8851e9f065c444bced
Chang, Liang <bigbrightf@gmail.com>
2013-04-26 16:42:53 +0800

    update 20130426

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ FBB_Data_Provider
 =================
 
 Node.js website to provide JSON data, and test for website deployment.
+
+返回JSON，但在另一个Site中进行跨域访问，会返回0状态，即不允许访问。
--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var url = require('url');
 http.createServer(function(req, res){
    var pathName = url.parse(req.url).pathname;
    console.log('Request for ' + pathName + ' received.');
-   res.writeHead(200, {'Content-Type': 'text/html'});
    if(pathName == '/getData'){
-      res.write('<h2>Get Data!!!</h2>');
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      //var games = {"Konami": "Metal Gear Solid", "Square": "Final Fantasy", "Bandai": "Super Robot War", "Enix": "Dragon Quest", "Ubisoft": "Assassin Creed", "Santa Monica": "God of War"};
+      var games = {"games":["Metal Gear Solid", "Final Fantasy", "Super Robot War", "Dragon Quest", "Assassin Creed", "God of War"]};
+      res.write(JSON.stringify(games));
+      res.end();
    }else{
+      res.writeHead(200, {'Content-Type': 'text/html'});
       res.write('<h1>FBB Data Provider</h1>');
+      res.end('<p>Hello FBB!</p>');
    }
-   res.end('<p>Hello FBB!</p>');
 }).listen(3000);
